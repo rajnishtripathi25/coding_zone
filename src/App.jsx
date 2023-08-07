849aedef89652f2e559d70e09f95c42723da3986
Rajnish Tripathi <rajnish.tripathi@unthinkable.co>
2023-08-07 18:32:29 +0530

    code zone landing page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle"
+import Home from './Home';
 import Navbar from './Navbar';
 import Service from './Service';
 import About from './About';
 import Contact from './Contact';
 import { Route, Routes, Navigate } from 'react-router-dom';
 import Footer from './Footer';
-import Home from './Home';
 
 const App = () => {
 
